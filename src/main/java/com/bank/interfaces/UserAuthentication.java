41fdfc2ac57b9c4f8e1baa1631764a27bd844674
Andres Acosta Garcia <andresfelipeacostagarcia34@gmail.com>
2023-11-19 13:34:13 -0500

    Modification with inetrfaces

--- src/main/java/com/bank/interfaces/UserAuthentication.java
+++ src/main/java/com/bank/interfaces/UserAuthentication.java
@@ -5,4 +5,8 @@ public interface UserAuthentication {
 	public int Authentication(String user, String password);
 	
 	public boolean checkUser(String usrName, String psswd);
+
+	public boolean checkU7er(String usrName, String psswd);
+	
+	
 }
